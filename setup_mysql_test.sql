534c73d1e4e2fba3bfd2cfb4b5e4648a88657eb8
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-22 01:13:02 +0000

    Added file for Task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- Prepares a MySQL server for the project.
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 ALTER USER 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
