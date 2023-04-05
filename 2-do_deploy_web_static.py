d424908002943ecb1fb146786b18ea79501a2d78
Fisayo Oladejo <108827240+FisayoOladejo@users.noreply.github.com>
2023-04-05 18:21:35 +0100

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+    Fabric script that distributes an archive to the web servers.
 """
+from os import path
+from fabric.api import env, put, run
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['35.229.40.200', '35.229.23.118']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
+    """ Function that distributes the archive.
+    Args:
+        archive_path (str): the path of the archive to deploy on the servers.
+    """
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        if not path.exists(archive_path):
+            raise FileNotFoundError
+
+        name = archive_path.split("/")[-1]
+        name_no_ext = name.split(".")[0]
+
+        remote = "/data/web_static/releases"
+        dest = "{}/{}".format(remote, name_no_ext)
+
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(dest))
+        run('tar -xzf /tmp/{} -C {}'.format(name, dest))
+        run('rm /tmp/{}'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
+        run('ln -s {}/ /data/web_static/current'.format(dest))
+
     except:
+        print("Error. Version deploy aborted")
         return False
+
+    print("New version deployed!")
+    return True
