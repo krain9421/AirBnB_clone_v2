a856656d204ba9d16923ee867549553f1ad11f6d
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-10 03:41:42 +0000

    Deploy Static 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-    Fabric script that distributes an archive to the web servers.
+    Distributing an archiave to the web servers.
 """
 from os import path
-from fabric.api import env, put, run
+from fabric.api import *
 
-env.hosts = ['35.229.40.200', '35.229.23.118']
+env.hosts = ['54.236.53.229', '18.204.7.180']
 
 
 def do_deploy(archive_path):
-    """ Function that distributes the archive.
+    """Distributes the archive to the servers
     Args:
         archive_path (str): the path of the archive to deploy on the servers.
     """
@@ -18,16 +18,16 @@ def do_deploy(archive_path):
         if not path.exists(archive_path):
             raise FileNotFoundError
 
-        name = archive_path.split("/")[-1]
-        name_no_ext = name.split(".")[0]
+        arc_path = archive_path.split("/")[-1]
+        arc = arc_path.split(".")[0]
 
         remote = "/data/web_static/releases"
-        dest = "{}/{}".format(remote, name_no_ext)
+        dest = "{}/{}".format(remote, arc)
 
         put(archive_path, '/tmp')
         run('mkdir -p {}/'.format(dest))
-        run('tar -xzf /tmp/{} -C {}'.format(name, dest))
-        run('rm /tmp/{}'.format(name))
+        run('tar -xzf /tmp/{} -C {}'.format(arc_path, dest))
+        run('rm /tmp/{}'.format(arc_path))
         run('mv {}/web_static/* {}/'.format(dest, dest))
         run('rm -rf {}/web_static'.format(dest))
         run('rm -rf /data/web_static/current')
