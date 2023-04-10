8aa3921025782240f7e6073a7f16614be7b30670
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-10 03:17:55 +0000

    Deploy Static 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,26 +2,24 @@
 # Sets up the web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
-apt update -y >/dev/null 2>&1
-apt install nginx -y >/dev/null 2>&1
+sudo apt update -y
+sudo apt install nginx -y
 
 # Create required directories if they don't exist
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Create fake index.html file
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tTesting Nginx Configuration\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
 
 # Create the symbolic link
-ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Change ownership of files and folders inside of /data folder
-chown -hR ubuntu:ubuntu /data
+# Give ownership of the /data folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data
 
 # Add alias to serve the content of /data/web_static/current to hbnb_static
-sed -i '51i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '51i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart the nginx service
 service nginx restart
