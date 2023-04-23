456b6dfbe335fb8abdeaf8b31315045f68ca2bf6
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-23 20:47:00 +0000

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,9 @@
 """Script that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
-
+app.run(host="0.0.0.0")
 
 @app.route('/', strict_slashes=False)
-@app.run(host="0.0.0.0")
 def hello_hbnb():
     """
     Returns the text `Hello HBNB!`
