8f5f7b8c94c31593721195048c44eb8c13f97948
Nwigwe Chidubem <krain9421@gmail.com>
2023-07-22 15:54:00 +0000

    Minimal Flask Application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,7 +2,6 @@
 """Script that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
-app.run(host="0.0.0.0")
 
 
 @app.route('/', strict_slashes=False)
@@ -11,3 +10,6 @@ def hello_hbnb():
     Returns the text `Hello HBNB!`
     """
     return ("Hello HBNB!")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
