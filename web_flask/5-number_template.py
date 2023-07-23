044339e4a363e8e69c8d4114dca9559b3a38357f
Nwigwe Chidubem <krain9421@gmail.com>
2023-07-23 10:43:13 +0000

    Rendering Templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,7 +52,7 @@ def show_number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def number_template(n=None):
     return render_template('5-number.html', n=n)
 
 
