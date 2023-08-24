60161596f0d5c003e8e30ea40497ccf97e6c4971
Nwigwe Chidubem <krain9421@gmail.com>
2023-08-24 14:15:39 +0000

    fixed console method 'destroy' for db storage

--- console.py
+++ console.py
@@ -10,7 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
-
+from os import getenv
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
@@ -233,7 +233,10 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del (storage.all()[key])
+            if getenv("HBNB_TYPE_STORAGE") == "db":
+                storage.delete(storage.all()[key])
+            else:
+                del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,7 +2,7 @@
 """Script that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -12,4 +12,5 @@ def hello_hbnb():
     return ("Hello HBNB!")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
+    # app.run(host="0.0.0.0", port=5000)
